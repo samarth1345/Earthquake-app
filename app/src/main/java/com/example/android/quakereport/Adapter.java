2bacbbea17ef52c1d3d1a8547ecb7589ec204092
Samarth Babbar <bsamarth56@gmail.com>
2021-10-06 00:19:19 +0530

    some changes done

--- app/src/main/java/com/example/android/quakereport/Adapter.java
+++ app/src/main/java/com/example/android/quakereport/Adapter.java
@@ -11,10 +11,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.SimpleTimeZone;
+import android.graphics.drawable.GradientDrawable;
 
 public class Adapter extends ArrayAdapter<news> {
 
@@ -23,32 +25,87 @@ public class Adapter extends ArrayAdapter<news> {
         super(context, 0, objects);
     }
 
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View currentview=convertView;
-        if(currentview == null)
-        {
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View currentview = convertView;
+        if (currentview == null) {
             currentview = LayoutInflater.from(getContext()).inflate(
                     R.layout.earthquake_layout, parent, false);
         }
-        news currentquake=getItem(position);
+        news currentquake = getItem(position);
 
-        TextView currentplace=(TextView)currentview.findViewById(R.id.place);
-        currentplace.setText(currentquake.getPlace());
+        TextView currentplace = (TextView) currentview.findViewById(R.id.place);
+        String place = currentquake.getPlace();
+        if (place.contains(" of ")) {
+            String[] parts = place.split(" of ");
+            currentplace.setText(parts[0] + " of" + "\n" + parts[1]);
+        }
 
         Date date = new Date(currentquake.getTime());
 
         SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy");
         String dateToDisplay = dateFormatter.format(date);
 
-        SimpleDateFormat timeformatter= new SimpleDateFormat("h:mm a");
-        String timeToDisplay= timeformatter.format(date);
+        SimpleDateFormat timeformatter = new SimpleDateFormat("h:mm a");
+        String timeToDisplay = timeformatter.format(date);
 
-        TextView currentDate=(TextView)currentview.findViewById(R.id.date);
-        currentDate.setText(dateToDisplay+"\n"+timeToDisplay);
+        TextView currentDate = (TextView) currentview.findViewById(R.id.date);
+        currentDate.setText(dateToDisplay + "\n" + timeToDisplay);
 
-        TextView currentmag=(TextView)currentview.findViewById(R.id.magnitude);
-        currentmag.setText(currentquake.getMagnitude()+"");
+        TextView currentmag = (TextView) currentview.findViewById(R.id.magnitude);
+        double magnitude=currentquake.getMagnitude();
+        GradientDrawable magnitudeCircle = (GradientDrawable) currentmag.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getcolor(magnitude);
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+        currentmag.setText(magnitude+"");
+
 
         return currentview;
+    }
+    public int getcolor(Double magnitude)
+    {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch(magnitudeFloor)
+        {
+            case 0:
+                magnitudeColorResourceId=android.R.color.white;
+                break;
+            case 1:
+                magnitudeColorResourceId=R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId=R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId=R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId=R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId=R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId=R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId=R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId=R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId=R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId=R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
--- app/src/main/java/com/example/android/quakereport/news.java
+++ app/src/main/java/com/example/android/quakereport/news.java
@@ -2,17 +2,23 @@ package com.example.android.quakereport;
 
 public class news {
     private String place;
+    private String url;
     private double magnitude;
     private long timeinMilliseconds;
-    news(String place,double magnitude,long timeinMilliseconds)
+    news(String place,double magnitude,long timeinMilliseconds,String url)
     {
         this.timeinMilliseconds=timeinMilliseconds;
         this.magnitude=magnitude;
         this.place=place;
+        this.url=url;
     }
     String getPlace()
     {
         return place;
+    }
+    String getUrl()
+    {
+        return url;
     }
     long getTime()
     {
